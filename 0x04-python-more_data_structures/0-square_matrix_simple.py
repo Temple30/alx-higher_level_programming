4124a2db1a66fd7ba737c0ce27f03182beaa4ee2
Temple30 <asuquogodwwin34@gmail.com>
2024-01-24 15:31:29 -0800

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,6 @@ def square_matrix_simple(matrix=[]):
 
     result_matrix = [[0] * len(row) for row in matrix]
 
-
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             result_matrix[i][j] = matrix[i][j] ** 2
