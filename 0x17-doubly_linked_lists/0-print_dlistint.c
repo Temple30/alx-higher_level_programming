b58bb62e70d99d2926ca572c3977fc2bb881e79e
Temple30 <asuquogodwwin34@gmail.com>
2024-01-25 06:18:27 -0800

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,11 +12,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	count = 0;
 
-	if (h==NULL)
+	if (h == NULL)
 		return (count);
 
-	while (h->prev !=NULL)
-		h =h->prev;
+	while (h->prev != NULL)
+		h = h->prev;
 
 	while (h != NULL)
 	{
