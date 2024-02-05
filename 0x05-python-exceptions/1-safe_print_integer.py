759c9245f3f6f3b1b7fdb85814aa67d0f3b2561e
Temple30 <asuquogodwwin34@gmail.com>
2024-02-05 12:39:16 -0800

    update

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return True
     except (ValueError, TypeError):
         return False
-    else:
-        print()  # Print a new line after the integer
-        return True
-
