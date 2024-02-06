645aa8bba64804deb0059715eaf9c645077da6d7
Temple30 <asuquogodwwin34@gmail.com>
2024-02-06 14:24:02 -0800

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,89 +1,76 @@
 #!/usr/bin/python3
-"""Singly-linked definition"""
+"""singly-linked defination"""
 
 
 class Node:
-    """Represents a node in a singly-linked list."""
+    """Node class body."""
 
     def __init__(self, data, next_node=None):
-        """Initialize a node.
-
+        """Node contructor.
         Args:
-            data (int): The data of the node.
-            next_node (Node, optional):
-            The next node in the list. Defaults to None.
+            data (int): The data of the new Node.
+            next_node (Node): The next node of the new Node.
         """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Getter and setter for the data of the node."""
-        return self.__data
+        """Setter and Getter of a Node."""
+        return (self.__data)
 
     @data.setter
     def data(self, value):
-        """Set the data of the node.
-
-        Args:
-            value (int): The new data of the node.
-
-        Raises:
-            TypeError: If value is not an integer.
-        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """Getter and setter for the next node."""
-        return self.__next_node
+        """Getter and Setter of a  Node."""
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
-        """Set the next node of the current node.
-
-        Args:
-            value (Node or None): The next node in the list.
-
-        Raises:
-            TypeError: If value is not a Node object or None.
-        """
         if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object or None")
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """Represents a singly-linked list."""
+    """Singly-linked defination"""
 
     def __init__(self):
-        """Initialize an empty singly-linked list."""
+        """SinglyLinkedList contructor."""
         self.__head = None
 
     def sorted_insert(self, value):
-        """Insert a new node into the list in sorted order.
-
+        """Insert a new Node to the SinglyLinkedList.
+        The node is inserted into the list at the correct
+        ordered numerical position.
         Args:
-            value (int): The value to insert into the list.
+            value (Node): The new Node to insert.
         """
-        new_node = Node(value)
-        if self.__head is None or self.__head.data >= value:
-            new_node.next_node = self.__head
-            self.__head = new_node
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
         else:
-            current = self.__head
-            while current.next_node and current.next_node.data < value:
-                current = current.next_node
-            new_node.next_node = current.next_node
-            current.next_node = new_node
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
 
     def __str__(self):
-        """Return a string representation of the linked list."""
+        """Define the print() representation of a SinglyLinkedList."""
         values = []
-        current = self.__head
-        while current:
-            values.append(str(current.data))
-            current = current.next_node
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
         return ('\n'.join(values))
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-"""Square class definition"""
+"""Square class defination"""
 
 
 class Square:
-    """Represents a square."""
+    """Square class body"""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize a square.
-        
+        """Square constructor .
         Args:
-            size (int): The size of the square's sides.
-            position (tuple): The position of the square.
+            size (int): The size of the new square.
+            square position (int, int): of Tupple.
         """
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Getter and setter for the size of the square."""
-        return self.__size
+        """Setter and getter of a  square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Set the size of the square.
-        
-        Args:
-            value (int): The size of the square's sides.
-        
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -39,40 +29,30 @@ class Square:
 
     @property
     def position(self):
-        """Getter and setter for the position of the square."""
-        return self.__position
+        """Getter and Setter for position of the square."""
+        return (self.__position)
 
     @position.setter
     def position(self, value):
-        """Set the position of the square.
-        
-        Args:
-            value (tuple): The position of the square.
-        
-        Raises:
-            TypeError: If value is not a tuple of 2 positive integers.
-        """
-        if (not isinstance(value, tuple) or len(value) != 2 or
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Calculate the area of the square.
-        
-        Returns:
-            int: The area of the square.
-        """
-        return self.__size * self.__size
+        """Return new area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
-        """Print the square."""
+        """Print the stdout the square with the character."""
         if self.__size == 0:
-            print()
+            print("")
             return
 
-        for _ in range(self.__position[1]):
-            print()
-        for _ in range(self.__size):
-            print(" " * self.__position[0], "#" * self.__size)
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
