ae3745443d25cf5dc7830fbcfd49af88979ca061
Temple30 <asuquogodwwin34@gmail.com>
2024-04-29 02:10:52 -0700

    update

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument
-"""
+"""Module that adds a new state to a MySQL database using SQLAlchemy."""
 import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    # Create the SQLAlchemy engine using the provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # Create a session factory
     Session = sessionmaker(bind=engine)
+
+    # Create a session object
     session = Session()
-    new_state = State(name='Louisiana')
-    session.add(new_state)
-    new_instance = session.query(State).filter_by(name='Louisiana').first()
-    print(new_instance.id)
+
+    # Create a new State object for Louisiana
+    louisiana = State(name="Louisiana")
+    # Add the new state to the session
+    session.add(louisiana)
+    # Commit the session to persist the changes
     session.commit()
+    # Print the ID of the newly added state
+    print(louisiana.id)
